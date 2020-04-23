1819f8f0ea238844832622b01ade5636f04608a3
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-04-23 21:54:04 +0300

    fixing problems step1

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,34 +2,32 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max_char = array.max
-        for i in 0..array.size - 1 do
-          array[i] = max_char if array[i].positive?
-        end
-        array
+        max_el = array.reduce(0) { |max, el| max > el ? max : el }
+        array.map { |el| el.positive? ? max_el : el }
       end
 
       def search(array, query)
-        if array.include?(query)
-          left = 0
-          right = array.size - 1
-          mid = (left + right) / 2
+        array.each do |el|
+          return binar_search(array, query) if el == query
+        end
+        -1
+      end
+
+      def binar_search(array, query)
+        left = 0
+        right = array.size - 1
+        mid = (left + right) / 2
 
           while array[mid] != query
-
             if array[mid] > query
               right = mid - 1
             else
               left = mid + 1
             end
             mid = (left + right) / 2
-
           end
 
-          mid
-        else
-          -1
-        end
+        mid
       end
     end
   end
