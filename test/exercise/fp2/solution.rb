c7d704924b7b2fc1386272157a19e1630c68de53
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-04-25 13:19:46 +0300

    my_each my_reduce

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,11 +5,19 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
+
+      def my_each(&func
+        return if self.empty?
+        first, *rest = self
+        func.call(first)
+        my_array = MyArray.new(rest)
+        my_array.my_each(&func)
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+
       end
 
       # Написать свою функцию my_compact
@@ -17,7 +25,12 @@ module Exercise
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = 0, &func)
+        return acc if self.empty?
+        first, *rest = self
+        acc = func.call(acc, first)
+        my_array = MyArray.new(rest)
+        my_array.my_reduce(acc, &func)
       end
     end
   end
