ab19a8de9a09525098071656f951e3994ec36de7
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-04-30 09:43:17 +0300

    solution/rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,24 +1,25 @@
 module Inatra
   class << self
-    @@handlers = {}
 
-    def method_missing(method_name, args, &block)
-      if method_name
-        @@handlers[method_name.to_s] = {}
-      else
-        super
-      end
-      @@handlers[method_name.to_s][args] = block.call
-    end
-    
     def routes(&block)
+      @routes = {}
       instance_eval(&block)
     end
 
     def call(env)
       method = env['REQUEST_METHOD'].downcase
       path = env['PATH_INFO']
-      @@handlers[method][path]
+
+      if @routes[method][path].nil?
+        [404, {}, 'Not Found']
+      else
+        @routes[method][path]
+      end
+    end
+
+    def method_missing(method_name, args, &block)
+      @routes[method_name.to_s] = {} if @routes[method_name.to_s].nil?
+      @routes[method_name.to_s][args] = block.call
     end
   end
 end
